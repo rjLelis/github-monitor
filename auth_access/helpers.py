27eb1f6f107c7cf838641b71314eb6e8a3e02bcc
rjLelis <renatojlelis@gmail.com>
2020-01-07 15:58:42 -0300

    Create helpers file for auth_access module

---
+++ auth_access/helpers.py
@@ -0,0 +1,26 @@
+from django.shortcuts import redirect
+from django.contrib.auth import mixins
+from rest_framework.authentication import BaseAuthentication
+from monitor.models import Profile
+
+
+def login_required(function):
+    def wrapper(request, **kwargs):
+        if 'access_token' not in request.session:
+            return redirect('auth:index')
+        return function(request, **kwargs)
+    return wrapper
+
+
+class GithubAuthentication(BaseAuthentication):
+
+    def authenticate(self, request):
+        if 'access_token' not in request.session:
+            return None
+
+        acces_token = request.session.get('access_token')
+        try:
+            profile = Profile.objects.get(acces_token=acces_token)
+            return (profile, None)
+        except Profile.DoesNotExist:
+            return None
