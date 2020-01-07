37f72c4269c853f5795b4cea950bbdd8a9bb1bf5
rjLelis <renatojlelis@gmail.com>
2020-01-07 15:52:18 -0300

    Add urls file for monitor module

--- github_monitor/urls.py
+++ github_monitor/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth_access.urls', namespace='auth')),
-    path('', include('monitor.urls', namespace='monitor'))
+    path('monitor/', include('monitor.urls', namespace='monitor')),
+    path('', include('frontend.urls', namespace='frontend'))
 ]
