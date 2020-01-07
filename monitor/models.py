001f12bf225af7c03e9a1848cb8633df45b1b1ce
rjLelis <renatojlelis@gmail.com>
2020-01-07 17:23:42 -0300

    Create Profile model

--- monitor/models.py
+++ monitor/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 class Profile(models.Model):
-    pass
+    username = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    access_token = models.CharField(max_length=40)
