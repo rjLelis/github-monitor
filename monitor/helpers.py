48d6990f795349351ef33182f2983c57f087887c
rjLelis <renatojlelis@gmail.com>
2020-01-08 15:01:52 -0300

    Change create_profile to hava create or update functionality

--- monitor/helpers.py
+++ monitor/helpers.py
@@ -1,16 +1,24 @@
 from .models import Profile
 
 
-def create_profile(**kwargs):
-    profile, created = Profile.objects.get_or_create(**kwargs)
-    return (created, profile)
+def create_profile(**profile):
+    profile, created = Profile.objects.get_or_create(**profile)
+    if not created:
+        profile.name = profile.get('username', profile.name)
+        profile.email = profile.get('email', profile.email)
+        profile.access_token = profile.get('access_token', profile.access_token)
+        profile.save()
+
+    return (profile, created)
 
 
 def get_profile(**login_or_token):
     try:
         profile = Profile.objects.get(**login_or_token)
-        return (True, profile)
+        found = True
+        return (profile, found)
     except Profile.DoesNotExist:
-        return (False, None)
+        found = False
+        return (None, found)
     except Exception as e:
         raise e
