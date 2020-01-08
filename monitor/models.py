9b59cc3f5b8bf45cf9e4f825f14a91bf82b2d27e
rjLelis <renatojlelis@gmail.com>
2020-01-08 14:59:24 -0300

    Create repository & commit models

--- monitor/models.py
+++ monitor/models.py
@@ -2,6 +2,42 @@ from django.db import models
 
 class Profile(models.Model):
     username = models.CharField(max_length=50, unique=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=True)
     email = models.EmailField()
-    access_token = models.CharField(max_length=40)
+    access_token = models.CharField(max_length=40, null=True)
+
+    def __str__(self):
+        return f'{self.name} <{self.username}>'
+
+
+class Repository(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(default='')
+    owner = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='owner'
+    )
+
+    def __str__(self):
+        return f'{self.owner.username}/{self.name}'
+
+
+class Commit(models.Model):
+    sha = models.CharField(max_length=40, unique=True)
+    commiter = models.ForeignKey(
+        Profile,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='commiter'
+    )
+    commited_at = models.DateTimeField()
+    message = models.TextField()
+    repository = models.ForeignKey(
+        Repository,
+        on_delete=models.CASCADE,
+        related_name='repository'
+    )
+
+    def __str__(self):
+        return f'{self.message} {self.sha[:7]}'
