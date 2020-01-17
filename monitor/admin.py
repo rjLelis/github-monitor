efe532bab9c3ff88f60ebada0297bf822c0019fb
rjLelis <renatojlelis@gmail.com>
2020-01-17 14:04:48 -0300

    Add read only fields

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Profile, Repository, Commit
 
-admin.site.register(Profile)
-admin.site.register(Repository)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('access_token', )
+
+
+@admin.register(Repository)
+class RepositoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('full_name', )
+
+
 admin.site.register(Commit)
