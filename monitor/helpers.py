ea8a9363bc0d54fdd8456415d0ee0c39df93d030
rjLelis <renatojlelis@gmail.com>
2020-01-17 14:14:10 -0300

    Add more helpers functions

--- monitor/helpers.py
+++ monitor/helpers.py
@@ -1,24 +1,81 @@
-from .models import Profile
+from django.db import utils as db_utils
+from github import Github
+from github.GithubException import UnknownObjectException
+
+from rest_framework import status
+
+from .models import Profile, Repository, Commit
 
 
-def create_profile(**profile):
-    profile, created = Profile.objects.get_or_create(**profile)
-    if not created:
-        profile.name = profile.get('username', profile.name)
-        profile.email = profile.get('email', profile.email)
-        profile.access_token = profile.get('access_token', profile.access_token)
-        profile.save()
+def create_profile(**kwargs):
+    try:
+        username = kwargs.pop('username')
+        profile, created = Profile.objects.get_or_create(
+            username=username, defaults=kwargs)
+        if not created:
+            profile.name = kwargs.get('name', profile.name)
+            profile.email = kwargs.get('email', profile.email)
+            profile.access_token = kwargs.get('access_token', profile.access_token)
+            profile.save()
 
-    return (profile, created)
+        return profile, created
+    except db_utils.IntegrityError:
+        raise (f'username can\'t be null', status.HTTP_400_BAD_REQUEST)
 
 
 def get_profile(**login_or_token):
+    # Todo: refactor to add 'message' and 'status' to exception
     try:
         profile = Profile.objects.get(**login_or_token)
-        found = True
-        return (profile, found)
+        return profile, status.HTTP_200_OK
     except Profile.DoesNotExist:
-        found = False
-        return (None, found)
+        raise Exception('User not found', status.HTTP_404_NOT_FOUND)
+
+
+def create_repository(profile, full_name_or_id):
+    try:
+        g = Github(profile.access_token)
+        repo = g.get_repo(full_name_or_id=full_name_or_id)
+        new_repository = Repository.objects.create(
+            name=repo.name,
+            description=repo.description,
+            owner=profile,
+        )
+
+        create_commits(repo, new_repository)
+
+        return new_repository
+
+    except db_utils.IntegrityError:
+        raise Exception('repo already on the list', status.HTTP_400_BAD_REQUEST)
+    except UnknownObjectException:
+        raise Exception('repo not found', status.HTTP_404_NOT_FOUND)
     except Exception as e:
-        raise e
+        raise e.args
+
+
+def create_commits(github_repo, repo_object):
+    # Todo: add query to get commits only from the last month
+    try:
+        commits = github_repo.get_commits()
+        commit_list = []
+        for commit in commits:
+            profile, _ = create_profile(
+                name=commit.author.name,
+                username=commit.author.login,
+                email=commit.author.email
+            )
+
+            new_commit = Commit(
+                sha=commit.sha,
+                commiter=profile,
+                commited_at=commit.commit.committer.date,
+                message=commit.commit.message,
+                repository=repo_object
+            )
+
+            commit_list.append(new_commit)
+
+        Commit.objects.bulk_create(commit_list)
+    except db_utils.IntegrityError:
+        raise Exception('commit already on the list', status.HTTP_400_BAD_REQUEST)
