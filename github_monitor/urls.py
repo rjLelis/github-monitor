818c382d62c1b2e1640a3ba6394713af668e87dd
rjLelis <renatojlelis@gmail.com>
2020-01-18 15:45:49 -0300

    Move monitor urls to 'api/'

--- github_monitor/urls.py
+++ github_monitor/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth_access.urls', namespace='auth')),
-    path('monitor/', include('monitor.urls', namespace='monitor')),
+    path('api/', include('monitor.urls', namespace='monitor')),
     path('', include('frontend.urls', namespace='frontend'))
 ]
