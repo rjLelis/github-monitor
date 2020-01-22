b2b6975b94be0d36b66089a97cc5964e5ab61c30
rjLelis <renatojlelis@gmail.com>
2020-01-22 17:45:46 -0300

    Add state

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,22 +2,35 @@ import React from "react";
 import Header from './Header';
 import RepositoryList from './RepositoryList';
 import CommitList from './CommitList';
+import RepositoryForm from "./RepositoryForm";
 
 class App extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            repositories: [],
+            commits: []
+        };
+    }
+
     render() {
+        const { commits, repositories } = this.state;
         return (
             <div>
-                <header className="site-header">
+                <header className="site-header bd-navbar">
                     <Header />
                 </header>
                 <main role="main" className="jumbotron">
                     <div className="row">
+                        <div className='col-md-12'>
+                            <RepositoryForm />
+                        </div>
                         <div className="col-md-4">
-                            <RepositoryList />
+                            <RepositoryList repositories={repositories} />
                         </div>
                         <div className="col-md-8">
-                            <CommitList />
+                            <CommitList commits={commits}/>
                         </div>
                     </div>
                 </main>
