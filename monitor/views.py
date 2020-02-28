8b2d3b955b9293a8e3b8bb0546f71558b1458b5e
rjLelis <renatojlelis@gmail.com>
2020-02-27 21:24:06 -0300

    Fix push_event return

--- monitor/views.py
+++ monitor/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http.response import HttpResponse
 
 from auth_access import helpers as auth_helpers
 
@@ -60,3 +61,5 @@ def push_event(request):
     monitor_task.insert_pushed_commits(
         repository_info, commits_pushed, sender
     )
+
+    return Response(status=status.HTTP_200_OK)
