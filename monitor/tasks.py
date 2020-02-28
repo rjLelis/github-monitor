43f0dec4e95c430e88662597410a1dd72e613bf5
rjLelis <renatojlelis@gmail.com>
2020-02-27 22:13:02 -0300

    Fix bug in insert_pushed_commits

--- monitor/tasks.py
+++ monitor/tasks.py
@@ -5,7 +5,7 @@ from .models import Commit
 
 
 @task()
-def insert_pushed_commits(repository, commits, sender):
+def insert_pushed_commits(repository, new_commits, sender):
     try:
         repo_full_name = repository.get('full_name')
         repository = monitor_helpers.get_repository_by_full_name(
@@ -15,7 +15,7 @@ def insert_pushed_commits(repository, commits, sender):
         profile = monitor_helpers.create_profile(username=sender_username)
 
         commits = []
-        for commit in commits:
+        for commit in new_commits:
             new_commit = Commit(
                 sha=commit.get('id'),
                 commiter=profile,
@@ -26,7 +26,7 @@ def insert_pushed_commits(repository, commits, sender):
             commits.append(new_commit)
 
         monitor_helpers.create_commits(commits)
-        print('Created')
+        return 'Commits created'
     except Exception as e:
         print('Error ', e)
         return None
